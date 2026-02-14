346b8a9933077098b2c61495de1c0b670a1d71d6
red095 <redietteklay8@gamil.com>
2026-02-14 12:00:30 +0300

    feat: Implement core game mechanics including player movement, car generation, and collision detection.

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,24 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager (Turtle):
+    def __init__(self):
+        super().__init__()
+        self.create_cars()
+        self.move_distance=STARTING_MOVE_DISTANCE
+    def create_cars(self):
+        self.color(random.choice(COLORS))
+        self.penup()
+        self.shape("square")
+        self.shapesize(stretch_len=2, stretch_wid=1, outline=1)
+        self.goto(270, random.randint(-230, 230))
+
+    def move(self):
+        self.backward(self.move_distance)
+
+
--- main.py
+++ main.py
@@ -7,8 +7,29 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player = Player()
+counter=0
+car_manager = CarManager()
+cars= [car_manager]
 game_is_on = True
 while game_is_on:
+    if player.ycor() > 280:
+        player.refresh()
+    if counter%6==0:
+        new_car = CarManager()
+        cars.append(new_car)
+    for car in cars:
+        car.move()
+        if player.distance(car) < 20:
+            game_is_on = False
+    counter+=1
     time.sleep(0.1)
     screen.update()
+    screen.listen()
+    screen.onkey(player.move, "Up")
+
+
+
+
+
+
